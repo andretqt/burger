96abd6f3577289b87a2e192bff84d2fd0199e041
yung dev <andre.torquati@gmail.com>
2018-07-31 17:13:21 -0400

    front-end pretty much done

--- public/js/views.js
+++ public/js/views.js
@@ -1,23 +1,41 @@
-//pressing submit button
+$(document).ready(function () {
 
-var submitButton = $('.submit-button');
-var listBeforeEating = $('#list-before');
-var burger_counter = 0;
-submitButton.on("click", function(e) {
-    event.preventDefault();
-    burger_counter += 1;
-    var burgerInput = $('#burger-name').val().trim();
-    var newBurger = $('<li>').html(burgerInput);
-    var devourButton = $('<button>').html('eat burger');
-    devourButton.addClass("devour-button");
-    newBurger.attr('data-id', burger_counter);
-    newBurger.attr('eaten', false);
-    newBurger.append(devourButton);
-    listBeforeEating.append(newBurger);
-    var anyDevourButton = $('.devour-button');
-    anyDevourButton.on("click", function(e){
-    event.preventDefault();
-    console.log('got pressed');
-    });
+    var submitButton = $('.submit-button');
+    var listBeforeEating = $('#list-before');
+    var burger_counter = 0;
+    var listAfterEating = $('#list-after');
+    // Adding event listeners for deleting, editing, and adding todos
+    $(document).on("click", "#create-burger", createBurger);
+    $(document).on("click", ".devour-button", devourBurger);
+    
+
+    function createBurger() {
+        event.preventDefault();
+        burger_counter += 1;
+        var burgerInput = $('#burger-name').val().trim();
+        var newBurger = $('<li>').html(burgerInput);
+        var devourButton = $('<button>').html('eat burger');
+        devourButton.addClass("devour-button");
+        devourButton.addClass("btn btn-danger")
+        newBurger.attr('data-id', burger_counter);
+        newBurger.attr('eaten', false);
+        newBurger.append(devourButton);
+        listBeforeEating.append(newBurger);
+    }
+
+    function devourBurger() {
+        event.preventDefault();
+        var closestListElt = $(this).closest("li");
+        //when appending a unique elt we don't have to delete it after (i.e. appending moves it, doesn't clone it)
+        closestListElt.attr('eaten', true);
+        listAfterEating.append(closestListElt);
+        $(this).remove();
+
+
+    }
+
 });
+
+
+
 
